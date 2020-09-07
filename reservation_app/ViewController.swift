5d93745a0c5d0b8bcf5abe3fd3587da5ce0e74e6
pokii-taka <61927184+pokii-taka@users.noreply.github.com>
2020-09-07 19:40:13 +0900

    Initial Commit

---
+++ reservation_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reservation_app
+//
+//  Created by 山名隆晃 on 2020/09/07.
+//  Copyright © 2020 Takaaki Yamana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
